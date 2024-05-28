b8eff33c36237549d9db765e9add61013b902cf4
arpittiwari <arpittiwari0811@gmail.com>
2024-05-28 17:11:23 +0530

    chore: Update npm dependencies and add dotenv, express, and mongoose

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
-import mongoose from "mongoose";
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
 
-(async()=>{})()
+dotenv.config({
+    path:'/.env'
+})
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+
+;(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",()=>{
+            console.log();
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on port on ${process.env.PORT}`)
+        })
+
+        
+    } catch (error) {
+        console.error("ERROR:", error)
+        throw err
+    }
+})()
+*/
