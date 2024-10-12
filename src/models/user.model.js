04c9078c1e4d8055d84a93e6b73d9176f287e4b2
arpittiwari <arpittiwari0811@gmail.com>
2024-10-13 01:53:51 +0530

    refactor: Update user model methods and token generation
    Refactor the user model in `user.model.js` to update the `verifyPassword` method and the `generateToken` method.
    
    - Update the `verifyPassword` method to use the `bcrypt.compare` function for password verification.
    - Update the `generateToken` method to include additional user information in the token payload, such as email and fullname.
    - Add the `ACCESS_TOKEN_EXPIRES_IN` constant for token expiration.
    
    These changes improve the security and functionality of the user model.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -55,18 +55,23 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-userSchema.methods.verifyPassword = async function (password){
+userSchema.methods.verifyPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
-}
+};
 
 userSchema.methods.generateToken = function () {
   return jwt.sign(
-    { _id: this._id, username: this.username },
-    process.env.ACCESS_TOKEN_SECRET,
     {
-      expiresIn: "7d",
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,   // secret key   
+    {
+      expiresIn: ACCESS_TOKEN_EXPIRES_IN,
     }
   );
-}
+};
 
 export const User = mongoose.model("User", userSchema);
