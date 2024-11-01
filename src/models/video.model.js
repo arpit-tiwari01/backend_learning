220405f1ce3cebf476cd9ac8cd3d984542ee68dc
arpittiwari <arpittiwari0811@gmail.com>
2024-11-01 15:41:12 +0530

    feat: Add getWatchHistory function to user.controller.js and update video model schema

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,62 +1,47 @@
-import { type } from "express/lib/response";
-import mongoose, { Schema } from "mongoose";
+import mongoose, {Schema} from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const videoSchema = new Schema({
-    title: {
-        type: String, //use cloudinary url
-        required: true,
-        trim: true,
-        index: true
-    },
-    description: {
-        type: String,
-        required: true,
-        trim: true,
-        index: true
-    },
-    duration: {
-        type: String,
-        required: true,
-    },
-    thumbnail: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    videoUrl: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    views: {
-        type: Number,
-        default: 0
-    },
-    isPublished: {
-        type: Boolean,
-        default: true,
-    },
-    likes: {
-        type: Number,
-        default: 0
-    },
-    dislikes: {
-        type: Number,
-        default: 0
-    },
-    // comments: [
-    //     {
-    //         type: Schema.Types.ObjectId,
-    //         ref: "Comment"
-    //     }
-    // ],
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, //cloudinary url
+            required: true
+        },
+        thumbnail: {
+            type: String, //cloudinary url
+            required: true
+        },
+        title: {
+            type: String, 
+            required: true
+        },
+        description: {
+            type: String, 
+            required: true
+        },
+        duration: {
+            type: Number, 
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+
+    }, 
+    {
+        timestamps: true
     }
-}, { timestamps: true });
+)
 
-videoSchema.plugin(mongooseAggregatePaginate);
+videoSchema.plugin(mongooseAggregatePaginate)
 
-export const Video = mongoose.model("Video", videoSchema);
+export const Video = mongoose.model("Video", videoSchema)
